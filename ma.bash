b623b9a6b7b878d85e8a7852b697f639200514b7
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-08-11 02:59:22 -0400

    commit 242121222 on 20210811

---
+++ ma.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash # Copyright 2019-2021 (c) all rights reserved
+# by BuildAPKs https://buildapks.github.io see LICENSE
+# File RDR/conf/README.md has more information about this file.
+#####################################################################
+set -Eeuo pipefail
+shopt -s nullglob globstar
+. "$RDR/scripts/bash/shlibs/trap.bash" 210 211 212
+cd "$JDR"
+# _AT_ username/repository commit
+_AT_ LukasKnuth/TinyStackDroid f1881294de65bf4294913988a6971d3f6fdc1db8
+# SDRsusty/buildAPKsWidgets ma.bash OEF
