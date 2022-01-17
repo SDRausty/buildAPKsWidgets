c6a0f93bef8160b5eca691d97b58f02200609ba9
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-01-17 15:10:52 -0500

    commit 734004811 on 20220117

--- ma.bash
+++ ma.bash
@@ -9,4 +9,8 @@ cd "$JDR"
 # _AT_ username/repository commit
 _AT_ hc5duke/ClockTzWidget 9494467279aeed215f0e2f8529981ded9de29e2c
 _AT_ LukasKnuth/TinyStackDroid f1881294de65bf4294913988a6971d3f6fdc1db8
+_AT_ samir5421/analog_clock_widget1 27ce768e4507751df02cf2512d0cacc507b64faa
+_AT_ samir5421/analog_clock_widget2 c9f1de9d484ffcb3b54e81ebcd1e5e8771ae34ba
+_AT_ samir5421/analog_clock_widget3 6c5c095deb6bf5bac220046b099df9e087bdf64d
+_AT_ samir5421/analog_clock_widget4 82d47dc1b3190079f40316a6fb3d219dd45f767b
 # SDRsusty/buildAPKsWidgets ma.bash OEF
