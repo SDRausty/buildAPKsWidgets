2bb0a5581890b5f4fea1c2cc8b2b1d873712688f
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-01-17 14:48:27 -0500

    commit 519635074 on 20220117

--- ma.bash
+++ ma.bash
@@ -7,5 +7,6 @@ shopt -s nullglob globstar
 . "$RDR/scripts/bash/shlibs/trap.bash" 210 211 212
 cd "$JDR"
 # _AT_ username/repository commit
+_AT_ hc5duke/ClockTzWidget 9494467279aeed215f0e2f8529981ded9de29e2c
 _AT_ LukasKnuth/TinyStackDroid f1881294de65bf4294913988a6971d3f6fdc1db8
 # SDRsusty/buildAPKsWidgets ma.bash OEF
